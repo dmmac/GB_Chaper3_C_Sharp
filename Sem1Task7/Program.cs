0d4439c1a63c5cd60b65b4730304dfd29b6b7c47
Dzmitry Masaliyeu <dmitry.masaliev@gmail.com>
2023-09-24 00:38:10 +0300

    Add task for Seminar 1: task 7

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine()??"0");
+
+if (number>100 && number<1000) 
+{
+    Console.WriteLine("Last digit is " + number % 10);
+}
+else
+{
+    Console.WriteLine("Provided number does not have three digits");
+}
