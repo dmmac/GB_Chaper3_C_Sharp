4f05eb168f19751d1206fcce9d5f8ad420f4004c
Dzmitry Masaliyeu <dmitry.masaliev@gmail.com>
2023-09-29 22:11:36 +0300

    Add seminar 1 tasks 6, 8

---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Enter the number: ");
+
+int a = int.Parse(Console.ReadLine()??"0");
+
+Console.WriteLine($"{a} -> {(a % 2 == 0 ? "Да" : "Нет")}");
---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter the number: ");
+
+int n = int.Parse(Console.ReadLine()??"0");
+
+Console.Write($"{n} -> ");
+
+for (int i = 2; i <= n; i += 2) 
+{
+    Console.Write(i);
+    if (i < n-1)
+    {
+        Console.Write(", ");
+    }
+}
