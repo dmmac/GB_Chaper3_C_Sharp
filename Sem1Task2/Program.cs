4ab170d059bcb7101d9a3af870e16606b944b6bc
Dzmitry Masaliyeu <dmitry.masaliev@gmail.com>
2023-09-29 21:49:54 +0300

    Add seminar 1 task 3

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter first number: ");
+
+int firstNumber = int.Parse(Console.ReadLine()??"0");
+
+Console.Write("Enter second number: ");
+
+int secondNumber = int.Parse(Console.ReadLine()??"0");
+
+Console.WriteLine($"a={firstNumber}; b={secondNumber} -> Max={Math.Max(firstNumber, secondNumber)}");
