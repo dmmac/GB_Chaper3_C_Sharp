286137d073384653bf8f1f410dc1311dddc7c6f6
Dzmitry Masaliyeu <dmitry.masaliev@gmail.com>
2023-09-29 21:53:48 +0300

    Add seminar 1 task 4

---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter first number: ");
+
+int a = int.Parse(Console.ReadLine()??"0");
+
+Console.Write("Enter second number: ");
+
+int b = int.Parse(Console.ReadLine()??"0");
+
+Console.Write("Enter third number: ");
+
+int c = int.Parse(Console.ReadLine()??"0");
+
+Console.WriteLine($"a={a}; b={b}; c={c} -> Max={Math.Max(Math.Max(a, b), c)}");
