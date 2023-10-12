b4c337781bd8f2dede9b60854c2d9e887e590e6c
Dzmitry Masaliyeu <dmitry.masaliev@gmail.com>
2023-10-12 22:14:04 +0300

    Add task 68 for seminar 9

---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,26 @@
+﻿int InputInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+int AkkermanFunction(int n, int m)
+{
+    if (n == 0) 
+    {
+        return m + 1;
+    }
+    else if (m == 0)
+    {
+        return AkkermanFunction(n - 1, 1);
+    } 
+    else
+    {
+        return AkkermanFunction(n-1, AkkermanFunction(n, m-1));
+    }
+}
+
+int n = InputInt("Please enter n number: ");
+int m = InputInt("Please enter m number: ");
+
+Console.WriteLine(AkkermanFunction(n,m));
