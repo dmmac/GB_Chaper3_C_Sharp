8bd00cef9b87e052fcbc5614b03cb109e80e29a9
Dzmitry Masaliyeu <dmitry.masaliev@gmail.com>
2023-10-12 21:35:17 +0300

    Add task 64 for seminar 9

---
+++ Sem9Task64/Program.cs
@@ -0,0 +1,23 @@
+﻿int InputInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+string GenerateNumbers(int n)
+{
+    if (n > 1) {
+        return n + ", " + GenerateNumbers(n-1);
+    } 
+    else if (n==1) 
+    {
+        return n.ToString();
+    } 
+    else 
+    {
+        return "";
+    }
+}
+
+int n = InputInt("Please enter n number: ");
+Console.WriteLine(GenerateNumbers(n));
