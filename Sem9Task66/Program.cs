71c16e11780c018e124ee0815f87326296f3a53c
Dzmitry Masaliyeu <dmitry.masaliev@gmail.com>
2023-10-12 21:43:32 +0300

    Add task 66 for seminar 9

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿int InputInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+int CalculateSumNM(int m, int n)
+{
+    if (m==n) {
+        return m;
+    } 
+    else 
+    {
+        return m + CalculateSumNM(m+1, n);
+    }
+}
+
+int m = InputInt("Please enter m number: ");
+int n = InputInt("Please enter n number: ");
+
+
+Console.WriteLine( m<n ? CalculateSumNM(m,n) : CalculateSumNM(n,m));
